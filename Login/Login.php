e8ceed71494cda9922134cd7553e7e54fa0d3ae5
Mahdi <i3130002@gmail.com>
2017-06-19 10:31:13 +0430

    Adding user login session.

--- Login/Login.php
+++ Login/Login.php
@@ -1,6 +1,10 @@
 ﻿<?php
 require_once "../Config.php";
 if ($users [$_REQUEST["username"]][0]==$_REQUEST["password"] ) {
+    session_start();
+    $_SESSION['username']=$_REQUEST["username"];
+    $_SESSION['role']=$users [$_REQUEST["username"]][1];
+
     echo 'correct';
 
 }
