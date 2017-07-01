26de2e595bb9a243d252e0b2eafcbedc2f7027ea
Mahdi <i3130002@gmail.com>
2017-07-01 17:03:18 +0430

    add coustomer and generate unique id card

--- Dashboard/coustomer/add/add.php
+++ Dashboard/coustomer/add/add.php
@@ -3,12 +3,14 @@
 //todo incomming data check
 
 //inseting data
-require_once ("../../../Config.php")
-$stmt = $connection->prepare('INSERT INTO coustomer (name, address, phone) VALUES (:name, :address, :phone)');
-
+require_once ("../../../Config.php");
+$stmt = $dbh->prepare('INSERT INTO coustomer (name, address, phone) VALUES (:name, :address, :phone)');
 $stmt->execute(array(':name' => $_POST['name'],':address' => $_POST['address'], ':phone' => $_POST['phone']));
 
-//getting inserted data+id
+//getting inserted id
+$stmt = $dbh->prepare('select id from coustomer where name=:name and address=:address and phone=:phone');
+$stmt->execute(array(':name' => $_POST['name'],':address' => $_POST['address'], ':phone' => $_POST['phone']));
+$id=$stmt->fetch();
 
 ?>
 <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
@@ -20,19 +22,19 @@ $stmt->execute(array(':name' => $_POST['name'],':address' => $_POST['address'], 
      <div  class="ccform">
     <div class="ccfield-prepend">
         <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
-        <input class="ccformfield" name="name" type="text" placeholder="نام و نام خانوادگی" required>
+        <input class="ccformfield" name="name" type="text" value="<?php echo $_POST['name']; ?>" >
     </div>
     <div class="ccfield-prepend">
         <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
-        <input class="ccformfield" name='address' type="text" placeholder="آدرس" required>
+        <input class="ccformfield" name='address' type="text" value="<?php echo $_POST['address']; ?>" >
     </div>
     <div class="ccfield-prepend">
         <span class="ccform-addon"><i class="fa fa-mobile-phone fa-2x"></i></span>
-        <input class="ccformfield" name='phone' type="text" placeholder="تلفن">
+        <input class="ccformfield" name='phone' type="text" value="<?php echo $_POST['phone']; ?>">
     </div>
 	<div class="ccfield-prepend">
         <span class="ccform-addon"><i class="fa fa-id-card fa-2x"></i></span>
-        <input class="ccformfield" name='id' type="text" placeholder="شماره">
+        <input class="ccformfield" name='id' type="text" value="<?php echo $id[0]; ?>">
     </div>
 
     <div class="ccfield-prepend">
