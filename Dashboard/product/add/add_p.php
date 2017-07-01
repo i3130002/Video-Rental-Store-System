2ab9a8dac975bdd39a0a85448db500d742788729
shaghayeghjalali96 <shaghayeghjalali96@gmail.com>
2017-07-01 22:55:25 +0430

    add product

---
+++ Dashboard/product/add/add_p.php
@@ -0,0 +1,28 @@
+﻿<?php 
+//todo security check
+//todo incomming data check
+//inseting data
+require_once ("../../../Config.php");
+$stmt = $dbh->prepare('INSERT INTO product (name, id, type,number) VALUES (:name, :id, :type, :number)');
+$stmt->execute(array(':name' => $_POST['name'],':id' => $_POST['id'], ':type' => $_POST['type'], ':number' => $_POST['number']));
+
+
+?>
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>محصول موفق ثبت شد</h1>
+</header>
+<div class="wrapper">
+     <div  class="ccform">
+   
+
+    <div class="ccfield-prepend">
+        <a class="ccbtn" href="../.." >داشبورد</a> 
+    </div>
+  </div>  
+</div>
+<div class="credit">
+
+
+</div>
---
+++ Dashboard/product/add/index_p.php
@@ -0,0 +1,33 @@
+﻿<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>فرم افزودن محصول</h1>
+</header>
+<div class="wrapper">
+    <form method="post" action="add_p.php" class="ccform">
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-sun-o fa-2x "></i></span>
+        <input class="ccformfield" name="name" type="text" placeholder="نام محصول" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-sun-o fa-2x "></i></span>
+        <input class="ccformfield" name='type' type="text" placeholder="نوع" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-sun-o fa-2x "></i></span>
+        <input class="ccformfield" name='number' type="text" placeholder="تعداد">
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-sun-o fa-2x "></i></span>
+        <input class="ccformfield" name='id' type="int" placeholder="شماره">
+    </div>
+
+    <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="ثبت">
+    </div>
+    </form>
+</div>
+<div class="credit">
+<p>پس از وارد کردن مشخصات دکمه ثبت را بزنید</p>
+
+</div>
