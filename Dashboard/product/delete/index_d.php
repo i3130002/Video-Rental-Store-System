42431401b3f196fe46be781a807b2898b93ad384
lenovo5070\zahra <za.salimiyan@gmail.com>
2017-07-04 18:25:53 +0430

    no message

---
+++ Dashboard/product/delete/index_d.php
@@ -0,0 +1,23 @@
+﻿<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>فرم حذف  محصول</h1>
+</header>
+<div class="wrapper">
+    <form method="post" action="remove titlee.php" class="ccform">
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+        <input class="ccformfield" name="name" type="text" placeholder="نام محصول" required>
+    </div>
+    
+    
+    <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="تایید">
+    </div>
+    </form>
+    
+</div>
+<div class="credit">
+<p>پس از وارد کردن نام محصول تایید کنید</p>
+
+</div>
---
+++ Dashboard/product/delete/remove titlee.php
@@ -0,0 +1,30 @@
+﻿<?php 
+//inseting data
+require_once ("../../../Config.php");
+$stmt = $dbh->prepare('DELETE from product where name = :name');
+$stmt->execute(array(':name' => $_POST['name']));
+
+
+?>
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>عنوان محصول با موفقیت حذف شد</h1>
+</header>
+<div class="wrapper">
+     <div  class="ccform">
+     <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+        <input class="ccformfield" name="name" type="text" value="<?php echo $_POST['name']; ?>" >
+    </div>
+  
+
+    <div class="ccfield-prepend">
+        <a class="ccbtn" href="../.." >داشبورد</a> 
+    </div>
+  </div>  
+</div>
+<div class="credit">
+
+
+</div>
