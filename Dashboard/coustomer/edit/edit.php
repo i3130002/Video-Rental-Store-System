14c63865ee6db62b8b60d6b66584d830d95a3e9c
lenovo5070\zahra <za.salimiyan@gmail.com>
2017-07-05 13:01:17 +0430

    no message

---
+++ Dashboard/coustomer/edit/edit.php
@@ -0,0 +1,68 @@
+﻿<?php 
+require_once ("../../../Config.php");
+
+$query = "SELECT * FROM coustomer where name = :name";
+$result = $dbh->prepare($query);
+$result->execute();
+$row = $result->fetchAll(PDO::FETCH_ASSOC);
+echo "<table border='1'>
+<tr>
+<th>name</th>
+<th>address</th>
+<th>phone</th>
+</tr>";
+
+while($row){
+     echo "<tr>";
+     echo "<td>" . $row['name'] . "</td>";
+     echo "<td>" . $row['address'] . "</td>";
+     echo "<td>" . $row['phone'] . "</td>";
+     echo "</tr>";
+}
+echo "</table>";
+
+
+
+
+$stmt = $dbh->prepare('INSERT INTO coustomer ( address, phone) VALUES ( :address, :phone) where name = :name');
+$stmt->execute(array(':address' => $_POST['address'], ':phone' => $_POST['phone']));
+
+//getting inserted id
+$stmt = $dbh->prepare('select id from coustomer where name=:name and address=:address and phone=:phone');
+$stmt->execute(array(':name' => $_POST['name'],':address' => $_POST['address'], ':phone' => $_POST['phone']));
+$id=$stmt->fetch();
+
+?>
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>کارت مشتری</h1>
+</header>
+<div class="wrapper">
+     <div  class="ccform">
+ //   <div class="ccfield-prepend">
+ //       <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+ //       <input class="ccformfield" name="name" type="text" value="<?php echo $_POST['name']; ?>" >
+ //   </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+        <input class="ccformfield" name='address' type="text" value="<?php echo $_POST['address']; ?>" >
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-mobile-phone fa-2x"></i></span>
+        <input class="ccformfield" name='phone' type="text" value="<?php echo $_POST['phone']; ?>">
+    </div>
+	<div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-id-card fa-2x"></i></span>
+        <input class="ccformfield" name='id' type="text" value="<?php echo $id[0]; ?>">
+    </div>
+
+    <div class="ccfield-prepend">
+        <a class="ccbtn" href="../.." >داشبورد</a> 
+    </div>
+  </div>  
+</div>
+//<div class="credit">
+<p>ویرایش اطلاعات با موفقیت انجام پذیرفت</p>
+
+//</div>
---
+++ Dashboard/coustomer/edit/index.php
@@ -0,0 +1,40 @@
+﻿<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>فرم ویرایش مشتری</h1>
+</header>
+<div class="wrapper">
+    <form method="post" action="edit.php" class="ccform">
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+        <input class="ccformfield" name="name" type="text" placeholder="  نام و نام خانوادگی" required>
+    </div>
+    <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="جستجوی مشخصات مشتری">
+    </div>
+    </form>
+</div>
+
+
+<div class="wrapper">
+    <form method="post" action="edit.php" class="ccform">
+
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+        <input class="ccformfield" name='address' type="text" placeholder="آدرس جدید" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-mobile-phone fa-2x"></i></span>
+        <input class="ccformfield" name='phone' type="text" placeholder="تلفن جدید">
+    </div>
+
+
+    <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="ثبت">
+    </div>
+    </form>
+</div>
+<div class="credit">
+<p>پس از وارد کردن مشخصات مشتری دکمه ثبت را بزنید</p>
+
+</div>
