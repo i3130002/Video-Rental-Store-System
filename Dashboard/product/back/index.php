1e2d793092dc579e2d94fdab4e32d94566d2d346
farzaneh1612 <nalchi.f@gmail.com>
2017-07-08 09:33:00 +0430

    Update index.php

--- Dashboard/product/back/index.php
+++ Dashboard/product/back/index.php
@@ -1,11 +1,25 @@
 <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 <link href="style.css" rel="stylesheet">
 <header class="ccheader">
-    <h1>فرم برگشت  محصول</h1>
+    <h1>فرم برگشت محصولر</h1>
 </header>
+
 <div class="wrapper">
-    <form method="post" action="remove titlee.php" class="ccform">
+    <form method="post" action="back.php" class="ccform">
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-info fa-2x"></i></span>
+        <input class="ccformfield" name="id" type="text" placeholder="کد محصول" required>
+    </div>
     <div class="ccfield-prepend">
         <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
-        <input class="ccformfield" name="name" type="text" placeholder="شماره محصول" required>
+        <input class="ccformfield" name="date_e" type="text" placeholder="تاریخ بازگشت محصول" required>
     </div>
+    <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="جستجو">
+    </div>
+    </form>
+</div>
+<div class="credit">
+<p>پس از وارد کردن و کد محصول دکمه ثبت را بزنید</p>
+
+</div>
