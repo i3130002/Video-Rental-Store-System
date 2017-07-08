411363582a40a2352cb0c016e5e10d6fa00740d2
farzaneh1612 <nalchi.f@gmail.com>
2017-07-08 10:38:57 +0430

    Create index.php

---
+++ Dashboard/product/rules/index.php
@@ -0,0 +1,34 @@
+<link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+<link href="style.css" rel="stylesheet">
+<header class="ccheader">
+    <h1>قوانین متفرقه </h1>
+</header>
+<div class="wrapper">
+    <form method="post" action="add.php" class="ccform">
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-user fa-2x"></i></span>
+        <input class="ccformfield" name="id" type="text" placeholder="شماره محصول" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+        <input class="ccformfield" name='name' type="text" placeholder="نام محصول" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+        <input class="ccformfield" name='time_rent' type="text" placeholder="مدت اجاره محصول" required>
+    </div>
+    <div class="ccfield-prepend">
+        <span class="ccform-addon"><i class="fa fa-envelope fa-2x"></i></span>
+        <input class="ccformfield" name='cost' type="text" placeholder="قیمت اجاره محصول" required>
+    </div>
+     <div class="ccfield-prepend">
+        <input class="ccbtn" type="submit" value="تایید">
+    </div>
+    </form>
+    
+</div>
+<div class="credit">
+<p>پس از وارد کردن قیمت واجاره تایید کنید</p>
+
+</div>
+    
